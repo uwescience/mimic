a73abcb3ad7e8afa3e5b8afb69d2bf4d4bffb1f6
Andrew Whitaker <whitaker@cs.washington.edu>
2014-06-19 11:43:41 -0700

    Enhance run.py to parse, interpret the query

--- run.py
+++ run.py
@@ -4,8 +4,30 @@
 
 import sys
 
+# Yuck: raco path must be prepended before '.'
+sys.path.insert(0 ,"./raco")
+
+from raco.myrialang import compile_to_json
+from raco.myrial.myrial_parser import *
+from raco.algebra import Sequence, Exec
+
+def flatten(plan):
+    if isinstance(plan, Sequence):
+        return plan.args[:]
+    else:
+        return [plan]
+
+def run_aql(op):
+    query = op.command
+    print 'Got a query: ' + query
+
 def run_query(q):
-    print 'running query: ' + q
+    plan = myrial_to_physical_plan(q)
+    ops = flatten(plan)
+    aql_ops = [op for op in ops if isinstance(op, Exec) and op.language=='AQL']
+
+    for aql_op in aql_ops:
+        run_aql(aql_op)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
